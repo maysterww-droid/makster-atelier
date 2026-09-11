dc84e2440982bbb58077f16d51cc209e122f82f0
maysterww-droid <maysterww@gmail.com>
2026-09-11 13:17:54 +0200

    Add clients to Quote navigation

--- apps/quote/src/components/app-shell.tsx
+++ apps/quote/src/components/app-shell.tsx
@@ -21,6 +21,7 @@ export function AppShell({ organizationName, role, plan = 'FREE', children }: Pr
           <Link className="navItem" href="/">Главная</Link>
           <Link className="navItem" href="/projects/new">Новый расчёт</Link>
           <Link className="navItem" href="/quotes">Предложения</Link>
+          <Link className="navItem" href="/clients">Клиенты</Link>
           <Link className="navItem" href="/price-book">Прайс-лист</Link>
           <span className="navItem disabled">Библиотека модулей</span>
           <span className="navItem disabled">Фурнитура</span>
